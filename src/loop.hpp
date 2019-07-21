abbb8c1de9a287f583a6f6205e5d1e37be7831a6
Lipka Boldizsár <lipkab@zoho.com>
2019-07-21 09:12:25 +0200

    Rename private auxiliary classes to start with _.

--- src/loop.hpp
+++ src/loop.hpp
@@ -4,8 +4,10 @@
 #include <util.hpp>
 
 namespace stencil {
+
+// loop
 template<u8 n, u8 m, typename T>
-struct loop_impl
+struct _loop_impl
 {
     template<class F>
     static void run(const std::array<T, n>& from,
@@ -14,13 +16,13 @@ struct loop_impl
                     std::array<T, n>& i)
     {
         for (i[m - 1] = from[m - 1]; i[m - 1] < to[m - 1]; ++i[m - 1]) {
-            loop_impl<n, m - 1, T>::run(from, to, func, i);
+            _loop_impl<n, m - 1, T>::run(from, to, func, i);
         }
     }
 };
 
 template<u8 n, typename T>
-struct loop_impl<n, 0, T>
+struct _loop_impl<n, 0, T>
 {
     template<class F>
     static void run(const std::array<T, n>&,
@@ -41,12 +43,13 @@ struct loop
          const F& func)
     {
         std::array<T, n> i;
-        loop_impl<n, n, T>::run(from, to, func, i);
+        _loop_impl<n, n, T>::run(from, to, func, i);
     }
 };
 
+// loop_with_counter
 template<u8 n, u8 m, typename T, typename C = u64, typename I = u64>
-struct loop_with_counter_impl
+struct _loop_with_counter_impl
 {
     template<class F>
     static void run(const std::array<T, n>& from,
@@ -58,14 +61,14 @@ struct loop_with_counter_impl
     {
         for (i[m - 1] = from[m - 1]; i[m - 1] < to[m - 1];
              ++i[m - 1], counter += increment[m - 1]) {
-            loop_with_counter_impl<n, m - 1, T, C, I>::run(
+            _loop_with_counter_impl<n, m - 1, T, C, I>::run(
                 from, to, counter, increment, func, i);
         }
     }
 };
 
 template<u8 n, typename T, typename C, typename I>
-struct loop_with_counter_impl<n, 0, T, C, I>
+struct _loop_with_counter_impl<n, 0, T, C, I>
 {
     template<class F>
     static void run(const std::array<T, n>&,
@@ -90,7 +93,7 @@ struct loop_with_counter
                       const F& func)
     {
         std::array<T, n> i;
-        loop_with_counter_impl<n, n, T, C, I>::run(
+        _loop_with_counter_impl<n, n, T, C, I>::run(
             from, to, counter, increment, func, i);
     }
 };
